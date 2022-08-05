7e6009d9a83e0b09ee8971e9e00da67e2bf1b83c
mi5ha6in <mi5ha6in@gmail.com>
2022-08-05 18:56:33 +0300

    refactoring the Tag component

--- src/components/tag/Tag.jsx
+++ src/components/tag/Tag.jsx
@@ -4,14 +4,14 @@ import style from "./tag.module.css";
 export const Tag = ({ color, size }) => {
   return (
     <>
-      <div>
-        <div
-          className={classnames(
-            style.tag,
-            style[`tag_color_${color}`],
-            style[`tag_size_${size}`]
-          )}
-        />
+      <div
+        className={classnames(
+          style.tag,
+          style[`tag_color_${color}`],
+          style[`tag_size_${size}`]
+        )}
+      >
+        <span className="visually-hidden">{`${color} tag`}</span>
       </div>
     </>
   );
