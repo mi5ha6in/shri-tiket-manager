989aafff74bb39d04754103ff512798b27e02130
mi5ha6in <mi5ha6in@gmail.com>
2022-08-05 21:59:22 +0300

    uses TextInput component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Tag } from "./components/Tag/Tag";
+import { TextInput } from "./components/TextInput/TextInput";
 
 function App() {
   return (
-    <Tag color='yellow' />
-  )
+    <>
+      <Tag color="yellow" />
+      <TextInput name='name' id='1234'  />
+    </>
+  );
 }
 
 export default App;
