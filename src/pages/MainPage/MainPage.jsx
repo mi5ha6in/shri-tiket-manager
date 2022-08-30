e2781127b8a6fdb82ae8086c8d230d1fd7a7176c
mi5ha6in <mi5ha6in@gmail.com>
2022-08-30 11:52:16 +0300

    adds style for main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,14 @@
 import { Filter } from "./components/Filter/Filter";
 import { data } from "../../data/data";
 import { Board } from "./components/Board/Board";
+import style from "./mainPage.module.css";
+import classnames from "classnames";
 
 export const MainPage = () => {
   return (
-    <>
-      <Filter />
+    <main className={classnames(style[`page`])}>
+      <Filter className={classnames(style[`page__filter`])} />
       <Board lists={data}></Board>
-    </>
+    </main>
   );
 };
