e8518e7ecc7a90b5c0738cdafa0e34b6b2154c8a
mi5ha6in <mi5ha6in@gmail.com>
2022-08-31 21:08:44 +0300

    рефакторинг на использование апи для данных

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,16 @@
-import { Filter } from "./components/Filter/Filter";
-import { data } from "../../data/data";
-import { Board } from "./components/Board/Board";
 import style from "./mainPage.module.css";
 import classnames from "classnames";
+import { Filter } from "./components/Filter/Filter";
+import { Board } from "./components/Board/Board";
+import { getAllTasks } from "../../serverApi/serverApi";
+
+const allTasks = getAllTasks();
 
 export const MainPage = () => {
   return (
     <main className={classnames(style[`page`])}>
       <Filter className={classnames(style[`page__filter`])} />
-      <Board lists={data}></Board>
+      <Board lists={allTasks}></Board>
     </main>
   );
 };
