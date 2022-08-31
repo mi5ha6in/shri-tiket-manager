238f4f0ced33f7ffd84de48dbe880999c42b293f
mi5ha6in <mi5ha6in@gmail.com>
2022-09-01 00:35:57 +0300

    refactoring

--- src/pages/MainPage/components/ModalTicketEditor/ModalTicketEditor.jsx
+++ src/pages/MainPage/components/ModalTicketEditor/ModalTicketEditor.jsx
@@ -2,14 +2,11 @@ import { Modal } from "../../../../components/Modal/Modal";
 import { TicketEditor } from "../../../../components/TicketEditor/TicketEditor";
 
 export const ModalTicketEditor = ({ showModal, onClickCloseButton, title }) => {
-  const modalElement = showModal ? (
+  const modalElement = showModal && (
     <Modal onCloseClick={onClickCloseButton}>
       <TicketEditor title={title} />
     </Modal>
-  ) : null;
-  return (
-    <>
-      {modalElement}
-    </>
   );
+
+  return <>{modalElement}</>;
 };
